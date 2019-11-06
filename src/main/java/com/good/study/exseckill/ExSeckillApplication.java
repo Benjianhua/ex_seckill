86b3a6e4d4619993e661f0fd2b9449cb4ca9eef8
jianhuagu <763663928@qq.com>
2019-11-06 20:50:34 +0800

    初始化项目

---
+++ src/main/java/com/good/study/exseckill/ExSeckillApplication.java
@@ -0,0 +1,13 @@
+package com.good.study.exseckill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExSeckillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExSeckillApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/good/study/ex_seckill/ExSeckillApplicationTests.java
@@ -0,0 +1,13 @@
+package com.good.study.ex_seckill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExSeckillApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
